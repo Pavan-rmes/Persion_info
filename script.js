19fe9c73a2ff250fa20252c45a429d1212852887
Pavan-rmes <62424109+Pavan-rmes@users.noreply.github.com>
2021-09-12 12:04:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var img = document.querySelector(".persion_img")
+var Persion_info = document.querySelector(".persion_info")
+
+function fun()
+{
+    fetch("https://randomuser.me/api/")
+    .then(response=>response.json())
+    .then(data=>{
+        var info = data.results[0]
+        var name = info.name.first+" " +info.name.last
+        var Age = info.dob.age
+        var city = info.location.city
+        Persion_info.innerHTML = `name : ${name} <br> Age : ${Age} <br> city : ${city}`
+        img.src =  info.picture.large
+    })
+}
